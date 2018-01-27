a0a844320f0550e567ccdae5eecf166a71cee4b0
Amara BARGOUGUI <oelbargougui@gmail.com>
2018-01-27 14:38:23 +0100

    working with beans

--- src/main/java/com/abargougui/springboottraining/SpringBootTrainingApplication.java
+++ src/main/java/com/abargougui/springboottraining/SpringBootTrainingApplication.java
@@ -1,12 +1,60 @@
 package com.abargougui.springboottraining;
 
+import java.time.LocalDateTime;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class SpringBootTrainingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootTrainingApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(SpringBootTrainingApplication.class, args);
+		for (String name : context.getBeanDefinitionNames()) {
+			System.out.println("Bean: " + name);
+		}
+	}
+}
+
+@Component
+class DatePrinter {
+
+	@Autowired
+	private TimeFactory timeFactory;
+
+	public String printDate() {
+		return "Now it is " + timeFactory.now();
+	}
+
+	@PostConstruct
+	public void posy() {
+		System.out.println("output: " + printDate());
+	}
+}
+
+class TimeFactory {
+	public LocalDateTime now() {
+		return LocalDateTime.now();
+	}
+}
+
+@Configuration
+class myConfiguration {
+	@Bean
+	public TimeFactory timeFactory() {
+		return new TimeFactory();
+	}
+
+	@PostConstruct
+	public void post() {
+		System.out.println("Instance: " + timeFactory());
+		System.out.println("Instance: " + timeFactory());
 	}
 }
--- src/test/java/com/abargougui/springboottraining/SpringBootTrainingApplicationTests.java
+++ src/test/java/com/abargougui/springboottraining/SpringBootTrainingApplicationTests.java
@@ -1,8 +1,15 @@
 package com.abargougui.springboottraining;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.BDDMockito.given;
+
+import java.time.LocalDateTime;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -11,6 +18,18 @@ public class SpringBootTrainingApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Autowired
+	DatePrinter datePrinter;
+
+	@MockBean
+	TimeFactory timeFactory;
+
+	@Test
+	public void datePrinterWorks() {
+		given(timeFactory.now()).willReturn(LocalDateTime.of(2018, 01, 27, 14, 19, 4));
+		assertEquals("Now it is 2018-01-27T14:19:04", datePrinter.printDate());
 	}
 
 }
